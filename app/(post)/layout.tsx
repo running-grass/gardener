0ba10e692051b300566b87761474626a4977702d
Leo Liu <467195537@qq.com>
2023-02-13 15:07:54 +0800

    feat: 增加footer和rss链接

--- app/(post)/layout.tsx
+++ app/(post)/layout.tsx
@@ -1,6 +1,7 @@
 import dayjs from "dayjs";
 import relativeTime from 'dayjs/plugin/relativeTime';
 import 'dayjs/locale/zh-cn';
+
 import './globals.css';
 import { AnalyticsWrapper } from './components/analytics';
 import { generateRssFeed } from "../../lib/rss";
@@ -9,6 +10,17 @@ dayjs.extend(relativeTime);
 dayjs.locale('zh-cn');
 
 generateRssFeed();
+
+function Footer() {
+  const rssImage = "https://cdn.sspai.com/2020/08/26/article/7aa30a38350fe1111ab32aeb5bc87aa5?imageView2/2/w/1120/q/90/interlace/1/ignore-error/1";
+  return <footer>
+    <p className="flex justify-center items-center">
+
+    Copyright © 2023-Now · Powered by Next.js · <a target="_blank" href="/rss/feed.xml"><img alt="rss" src={rssImage} /></a>
+    </p>
+  </footer>
+}
+
 export default function RootLayout({
   children,
 }: {
@@ -17,9 +29,10 @@ export default function RootLayout({
   return (
     <html lang="zh">
       <head />
-      <body className="p-4 prose lg:prose-xl" >
+      <body className="p-4 prose lg:prose-xl m-auto" >
         {children}
         <AnalyticsWrapper />
+        <Footer />
       </body>
     </html>
   )
